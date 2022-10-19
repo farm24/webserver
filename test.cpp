7b2ed6cfe24ec9fbba1bcde349ef701cc5b59f76
farm24 <cgb41773@gmail.com>
2022-10-19 14:48:38 -0500

    test

--- test.cpp
+++ test.cpp
@@ -1,11 +1,8 @@
-#include <drogon/drogon.h>
-using namespace drogon;
-int main()
-{
-    app().setLogPath("./")
-         .setLogLevel(trantor::Logger::kWarn)
-         .addListener("0.0.0.0", 80)
-         .setThreadNum(16)
-         .enableRunAsDaemon()
-         .run();
+#include <iostream>
+using namespace std:
+
+int main() {
+    while (int i = 0; i < 100; i++) {
+        cout << i << "\n":
+    }
 }
